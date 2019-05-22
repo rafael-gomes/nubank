5b7dc19e0f39137e7e2fe61eb3360b30b158d713
Rafael Gomes <rafaell.gomes@gmail.com>
2019-05-22 01:51:13 -0300

    Criaçao do menu e estilizaçao. Criaçao e estilizaçao dos cards.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,74 @@
 import React from 'react';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Animated } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
+import Menu from '~/components/Menu';
 
-import { Container } from './styles';
+import {
+  Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation,
+} from './styles';
 
 export default function Main() {
+  const translateY = new Animated.Value(0);
+
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        }
+      }
+    ],
+    { useNativeDriver: true },
+  )
+
+  function onHandlerStateChanged(event) {
+
+  }
+
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Menu />
+
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChanged}
+        >
+
+          <Card style={{
+            transform: [{
+              translateY: translateY.interpolate({
+                inputRange: [-350, 0, 380],
+                outputRange: [-50, 0, 380],
+                extrapolate: 'clamp',
+              }),
+            }],
+          }}>
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
+            <CardContent>
+              <Title>Saldo disponível</Title>
+              <Description>R$ 197.985,25</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Transferência de R$ 20,00 recebida de Thomas Almeida hoje às 20:00h
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
+      </Content>
+
       <Tabs />
     </Container>
   );

9f95274b5ceba9efad897079fc876b629ef0402f
Rafael Gomes <rafaell.gomes@gmail.com>
2019-09-24 21:01:35 -0300

    Finalizaçao das animaçoes dos menus.

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -6,9 +6,15 @@ import QRCode from 'react-native-qrcode';
 
 import { Container, Code, Nav, NavItem, NavText, SignOutButton, SignOutButtonText } from './styles';
 
-export default function Menu() {
+export default function Menu({ translateY }) {
   return (
-    <Container>
+    <Container style={{
+      opacity: translateY.interpolate({
+        inputRange: [0, 150],
+        outputRange: [0, 1],
+      }),
+    }}
+    >
       <Code>
         <QRCode
           value='https://rocketseat.com.br'
@@ -40,6 +46,6 @@ export default function Menu() {
       <SignOutButton onPress={() => { }}>
         <SignOutButtonText>SAIR DO APP</SignOutButtonText>
       </SignOutButton>
-    </Container>
+    </Container >
   );
 }
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -14,6 +14,7 @@ import {
 } from './styles';
 
 export default function Main() {
+  let offset = 0;
   const translateY = new Animated.Value(0);
 
   const animatedEvent = Animated.event(
@@ -21,22 +22,49 @@ export default function Main() {
       {
         nativeEvent: {
           translationY: translateY,
-        }
-      }
+        },
+      },
     ],
     { useNativeDriver: true },
-  )
+  );
 
   function onHandlerStateChanged(event) {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      let opened = false;
+      const { translationY } = event.nativeEvent;
 
+      offset += translationY;
+
+      if (translationY >= 100) {
+        opened = true;
+
+
+      } else {
+        translateY.setValue(offset);
+        translateY.setOffset(0);
+        offset = 0;
+      }
+
+      Animated.timing(translateY, {
+        toValue: opened ? 380 : 0,
+        duration: 200,
+        useNativeDriver: true,
+
+      }).start(() => {
+        offset = opened ? 380 : 0;
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+      });
+    }
   }
+
 
   return (
     <Container>
       <Header />
 
       <Content>
-        <Menu />
+        <Menu translateY={translateY} />
 
         <PanGestureHandler
           onGestureEvent={animatedEvent}
@@ -69,7 +97,7 @@ export default function Main() {
         </PanGestureHandler>
       </Content>
 
-      <Tabs />
+      <Tabs translateY={translateY} />
     </Container>
   );
 }
